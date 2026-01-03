ea6aeb7b2258651c7bcc8135b52aa43b73edbc41
hamdanahmadkhan101-tech <hamdanahmadkhan101@gmail.com>
2026-01-03 19:20:42 +0500

    feat: add user registration route and controller

--- src/app.js
+++ src/app.js
@@ -16,8 +16,16 @@ app.use(express.json({ limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
-// Routes will go here
-// app.use('/api/v1/users', userRoutes);
+// Routes imports will go here
+import userRoutes from './routes/user.routes.js';
+
+
+
+
+
+// Routes usage will go here
+app.use('/api/v1/users', userRoutes);
+
 
 // Error middleware must be LAST
 app.use(errormiddleware);
